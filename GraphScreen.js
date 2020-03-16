57e027bcc1192743b18d6aab298d0a569b6a934b
Hugo Medina <rhadammanthis@gmail.com>
2020-03-16 21:03:47 +0000

    Touched up a few indicators in the chart plus very simple refactoring

--- GraphScreen.js
+++ GraphScreen.js
@@ -1,49 +1,65 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { LineChart, Grid, YAxis, XAxis } from 'react-native-svg-charts'
-import { Defs, LinearGradient, Stop, Circle, Text, Rect, G } from 'react-native-svg'
+import { Defs, LinearGradient, Stop, Circle, Text as SVGText, Rect, G } from 'react-native-svg'
 import {
   Button,
   StyleSheet,
   View,
-  Modal,
+  Text,
   FlatList,
   SafeAreaView,
 } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 const GraphScreen = props => {
 
-  const casesIceland = [{ cases: 0, death: 0, date: 1583020800000 },
-  { cases: 2, death: 0, date: 1583107200000 },
-  { cases: 9, death: 0, date: 1583193600000 },
-  { cases: 16, death: 0, date: 1583280000000 },
-  { cases: 26, death: 0, date: 1583366400000 },
-  { cases: 26, death: 0, date: 1583452800000 },
-  { cases: 45, death: 0, date: 1583539200000 },
-  { cases: 45, death: 0, date: 1583625600000 },
-  { cases: 55, death: 0, date: 1583712000000 },
-  { cases: 55, death: 0, date: 1583798400000 },
-  { cases: 61, death: 0, date: 1583884800000 },
-  { cases: 61, death: 0, date: 1583971200000 },
-  { cases: 61, death: 0, date: 1584057600000 },
-  { cases: 61, death: 0, date: 1584144000000 },
-  ]
-  const casesNetherlands = [{ cases: 7, death: 0, date: 1583020800000 },
-  { cases: 13, death: 0, date: 1583107200000 },
-  { cases: 18, death: 0, date: 1583193600000 },
-  { cases: 28, death: 0, date: 1583280000000 },
-  { cases: 38, death: 0, date: 1583366400000 },
-  { cases: 82, death: 0, date: 1583452800000 },
-  { cases: 128, death: 0, date: 1583539200000 },
-  { cases: 188, death: 0, date: 1583625600000 },
-  { cases: 265, death: 0, date: 1583712000000 },
-  { cases: 321, death: 0, date: 1583798400000 },
-  { cases: 382, death: 0, date: 1583884800000 },
-  { cases: 503, death: 0, date: 1583971200000 },
-  { cases: 614, death: 0, date: 1584057600000 },
-  { cases: 804, death: 0, date: 1584144000000 },
+  //Hardcoded dataset
+  const data = [
+    {
+      country: 'Iceland',
+      cases: [{ cases: 0, date: 1583020800000 },
+      { cases: 2, date: 1583107200000 },
+      { cases: 9, date: 1583193600000 },
+      { cases: 16, date: 1583280000000 },
+      { cases: 26, date: 1583366400000 },
+      { cases: 26, date: 1583452800000 },
+      { cases: 45, date: 1583539200000 },
+      { cases: 45, date: 1583625600000 },
+      { cases: 55, date: 1583712000000 },
+      { cases: 55, date: 1583798400000 },
+      { cases: 61, date: 1583884800000 },
+      { cases: 61, date: 1583971200000 },
+      { cases: 61, date: 1584057600000 },
+      { cases: 61, date: 1584144000000 },]
+    },
+    {
+      country: 'The Netherlands',
+      cases: [{ cases: 7, date: 1583020800000 },
+      { cases: 13, date: 1583107200000 },
+      { cases: 18, date: 1583193600000 },
+      { cases: 28, date: 1583280000000 },
+      { cases: 38, date: 1583366400000 },
+      { cases: 82, date: 1583452800000 },
+      { cases: 128, date: 1583539200000 },
+      { cases: 188, date: 1583625600000 },
+      { cases: 265, date: 1583712000000 },
+      { cases: 321, date: 1583798400000 },
+      { cases: 382, date: 1583884800000 },
+      { cases: 503, date: 1583971200000 },
+      { cases: 614, date: 1584057600000 },
+      { cases: 804, date: 1584144000000 },]
+    }
   ]
 
-  const [chartData, setChartData] = useState(casesIceland)
+  //Looks up a country in the dataset and returns the first instance
+  const retrieveData = (country) => {
+    let result = data.filter(obj => {
+      return obj.country === country
+    })
+    return result[0]
+  }
+
+  //Used to dynamically change the chart's data
+  const [chartData, setChartData] = useState(retrieveData('Iceland'))
 
   const Gradient = () => (
     <Defs key={'gradient'}>
@@ -59,28 +75,42 @@ const GraphScreen = props => {
         key={index}
         cx={x(value.date)}
         cy={y(value.cases)}
-        r={7}
+        r={4}
         stroke={'rgb(134, 65, 244)'}
         fill={'white'}
       />
     ))
   }
 
-  const CUT_OFF = 20
+  //Use to calculate when should the tooltip be drawn above or below the decorator
+  const calcCutOff = (arr) => {
+    let min = arr[0].cases, max = arr[0].cases;
+
+    for (let i = 1, len = arr.length; i < len; i++) {
+      let v = arr[i].cases;
+      min = (v < min) ? v : min;
+      max = (v > max) ? v : max;
+    }
+
+    return (max - min) * 0.5
+  }
+
+  const CUT_OFF = calcCutOff(chartData.cases)
+
   const Labels = ({ x, y, bandwidth, data }) => (
     data.map((value, index) => (
       <G
         key={index}>
         <Rect
-          x={x(value.date) - 10}
+          x={x(value.date) - (10 + value.cases.toString().length * 5) / 2}
           y={value.cases < CUT_OFF ? y(value.cases) - 27.5 : y(value.cases) + 2.5}
-          width={20}
+          width={10 + value.cases.toString().length * 5}
           height="25"
           strokeWidth="1"
           fill='rgb(66, 194, 244)'>
 
         </Rect>
-        <Text
+        <SVGText
           x={x(value.date)}
           y={value.cases < CUT_OFF ? y(value.cases) - 15 : y(value.cases) + 15}
           fontSize={14}
@@ -89,7 +119,7 @@ const GraphScreen = props => {
           textAnchor={'middle'}
         >
           {value.cases}
-        </Text>
+        </SVGText>
       </G>
     ))
   )
@@ -100,32 +130,34 @@ const GraphScreen = props => {
   }
 
   const axesSvg = { fontSize: 10, fill: 'grey' };
-  const verticalContentInset = { top: 10, bottom: 10 }
+  const verticalContentInset = { top: 20, bottom: 20 }
+  const chartContentInset = { top: 20, bottom: 20, left: 15, right: 25 }
   const xAxisHeight = 30
 
   return <View style={{ flex: 1 }}>
-    <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-      <Button title={"Iceland"} onPress={() => setChartData(casesIceland)}></Button>
-      <Button title={"Netherlands"} onPress={() => setChartData(casesNetherlands)}></Button>
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 5 }}>
+      <Button title={"Iceland"} onPress={() => setChartData(retrieveData('Iceland'))}></Button>
+      <Button title={"Netherlands"} onPress={() => setChartData(retrieveData('The Netherlands'))}></Button>
     </View>
-    <View style={{ flex: 1, flexDirection: 'row', paddingLeft: 10 }}>
+    <Text style={{ textAlign: 'center', fontSize: 20 }}>Coronavirus cases in {chartData.country}</Text>
+    <View style={styles.container}>
       <YAxis
-        data={chartData}
+        data={chartData.cases}
         yAccessor={({ item }) => item.cases}
         style={{ marginBottom: xAxisHeight }}
         contentInset={verticalContentInset}
         svg={axesSvg}
         numberOfTicks={10}
       />
-      <ScrollView horizontal={true} style={{ flex: 1 }}>
-        <View style={{ width: 500 }}>
+      <ScrollView horizontal={true} >
+        <View>
           <LineChart
             yAccessor={({ item }) => item.cases}
             xAccessor={({ item }) => item.date}
             animate={true}
-            style={{ height: 500 }}
-            data={chartData}
-            contentInset={{ top: 20, bottom: 20, left: 15, right: 15 }}
+            style={{ flexGrow: 1, width: chartData.cases.length * 50 }}
+            data={chartData.cases}
+            contentInset={chartContentInset}
             svg={{
               strokeWidth: 2,
               stroke: 'url(#gradient)',
@@ -137,10 +169,10 @@ const GraphScreen = props => {
             <Gradient />
           </LineChart>
           <XAxis
-            numberOfTicks={5}
-            style={{ marginHorizontal: 10, height: xAxisHeight }}
+            contentInset={chartContentInset}
+            style={{ height: xAxisHeight }}
             xAccessor={({ item }) => item.date}
-            data={chartData}
+            data={chartData.cases}
             svg={axesSvg}
             formatLabel={(value) => formatDate(value)}
           />
@@ -149,5 +181,13 @@ const GraphScreen = props => {
     </View>
   </View>
 }
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    flexDirection: 'row', 
+    paddingLeft: 10 
+  }
+});
 
 export default GraphScreen;
